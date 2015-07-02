25dd9a5666ccedb658eb9659e5895b8aa479eb13
Luke Savage <luke.savage92@gmail.com>
2015-07-02 17:52:44 -0500

    Implemented prototypal  enqueue and dequeue functions.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.nextUp = 0;
+  newQueue.index = 0;
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.index] = value;
+    this.index++;
+  },
+
+  dequeue: function() {
+
+  },
+
+  size: function() {
+    return this.index - this.nextUp;
+  }
+};
 
 
