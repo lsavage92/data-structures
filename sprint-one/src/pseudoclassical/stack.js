a137738ceb1058e780149dbaf677c34b25545b1e
Luke Savage <luke.savage92@gmail.com>
2015-07-02 18:37:42 -0500

    Implemented psuedoclassical stack. Added .push() and .size() to the prototype

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.index = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
 
+Stack.prototype.pop = function() {
+
+};
+
+Stack.prototype.size = function() {
+  return this.index;
+};
