c34befc6e98408c678556e9e43da728857e74a55
Luke Savage <luke.savage92@gmail.com>
2015-07-02 18:09:07 -0500

    Set up Queue. Added enqueue() and size() to Queue.prototype

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,16 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.nextUp = 0;
+  this.index = 0;
 };
 
+Queue.prototype.enqueue = function(value){
+  this.storage[this.index] = value;
+  this.index++;
+};
 
+Queue.prototype.size = function(){
+  return this.index - this.nextUp;
+};
