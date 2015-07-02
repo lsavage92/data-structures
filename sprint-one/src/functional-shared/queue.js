935d244dbd2436ae07638cf4fbcff65e20a0bd56
Luke Savage <luke.savage92@gmail.com>
2015-07-02 13:30:40 -0500

    Set up Queue class and added method stubs.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,27 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newQueue = {};
+  newQueue.storage = {};
+  newQueue.nextUp = 0;
+  newQueue.index = 0;
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+    enqueue : function(){
+
+    },
+    dequeue : function(){
+
+    },
+    size : function(){
+
+    }
+};
 
 
 
