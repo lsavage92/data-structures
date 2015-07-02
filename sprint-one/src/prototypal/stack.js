976dff80d8abd6dcd21d5b7d5e885382a54b2a51
Luke Savage <luke.savage92@gmail.com>
2015-07-02 17:44:24 -0500

    Implemented prototypal solutions for push and size.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods); //var newStack = stackMethods;
+  newStack.storage = {};
+  newStack.index = 0;
+
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.index] = value;
+    this.index++;
+  },
+
+  pop: function(){
+
+  },
+
+  size: function(){
+    return this.index;
+  }
+};
 
 
