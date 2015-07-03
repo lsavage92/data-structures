b75edb3e35d7123d66c3c1e0e38823f67cba5cc0
Luke Savage <luke.savage92@gmail.com>
2015-07-03 17:40:25 -0500

    Added .addNode() and .contains() implementations to Graph.prototype

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,25 @@
 
 
 var Graph = function(){
+  this.storage = {};
+
 };
 
 Graph.prototype.addNode = function(node){
+  this.storage[node] = node;
 };
 
 Graph.prototype.contains = function(node){
+  for(var key in this.storage) {
+    if(this.storage[key] === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.storage[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
@@ -28,5 +38,5 @@ Graph.prototype.forEachNode = function(cb){
  * Complexity: What is the time complexity of the above functions?
  */
 
-
+// {value: 3, edges: [{vale: 2, edges: [*first node*, ... ]}]}
 
