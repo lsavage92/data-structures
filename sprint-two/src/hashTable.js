8dfc1feb2e29d92031b642c0b2ad27a849d68600
Luke Savage <luke.savage92@gmail.com>
2015-07-04 13:59:58 -0500

    Implemented ._resize() for doubling when necessary.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,9 +1,11 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._numItems = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
+  this._numItems++;
   var i = getIndexBelowMaxForKey(k, this._limit);
   var tmp = this._storage.get(i);
   if( !tmp ){
@@ -12,6 +14,9 @@ HashTable.prototype.insert = function(k, v){
   else{
     tmp.push([k, v]);
     this._storage.set(i, tmp);
+  }
+  if(this._numItems >= this._limit * 3 / 4 ){
+    this._resize(this._limit * 2);
   }
 };
 //Time complexity O(1)
@@ -39,6 +44,20 @@ HashTable.prototype.remove = function(k){
 };
 //Time complexity O(1)
 
+HashTable.prototype._resize = function( size ){
+  var trueThis = this;
+  var tmpStorage = this._storage;
+  trueThis._numItems = 0;
+  trueThis._storage = LimitedArray(size);
+  trueThis._limit = size;
+  tmpStorage.each( function( bucket ) {
+    if( bucket ){
+      bucket.forEach( function( tuple ){
+        trueThis.insert(tuple[0], tuple[1]);
+      });
+    }
+  });
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
