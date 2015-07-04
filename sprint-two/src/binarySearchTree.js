ac8440ef273c93bf75fd33d6c595a8d049072c87
Luke Savage <luke.savage92@gmail.com>
2015-07-04 12:16:21 -0500

    Implemented the .insert() functions and attached to BinarySearchTree Prototype.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,34 @@
 var BinarySearchTree = function(value){
+  var newBinarySearchTree = Object.create(BinarySearchTree.setPrototype);
+
+  newBinarySearchTree.value = value;
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+
+  return newBinarySearchTree;
 };
 
+BinarySearchTree.setPrototype = {};
+
+BinarySearchTree.setPrototype.insert = function(value) {
+  if(value >= this.value && !this.right) {
+    this.right = BinarySearchTree(value);
+  } else if(value < this.value && !this.left) {
+    this.left = BinarySearchTree(value);
+  } else if(value >= this.value) {
+    this.right.insert(value);
+  } else {
+    this.left.insert(value);
+  }
+};
+
+BinarySearchTree.setPrototype.contains = function(value) {
+
+};
+
+BinarySearchTree.setPrototype.depthFirstLog = function(cb) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
