a90516b0bd9607e1adcc3c009fcd75a8f7566970
Luke Savage <luke.savage92@gmail.com>
2015-07-03 21:30:28 -0500

    Added time complexities for all functions implemented

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -9,6 +9,7 @@ var setPrototype = {};
 setPrototype.add = function(item){
   this._storage[item] = item;
 };
+//Time complexity O(1)
 
 setPrototype.contains = function(item){
   for( var key in this._storage ){
@@ -18,10 +19,12 @@ setPrototype.contains = function(item){
   }
   return false;
 };
+//Time complexity O(n)
 
 setPrototype.remove = function(item){
   delete this._storage[item];
 };
+//Time complexity O(1)
 
 /*
  * Complexity: What is the time complexity of the above functions?
